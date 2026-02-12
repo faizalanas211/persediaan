fb0c4a165c9a647377738e508cea6c0f3ac22c7a
faizalanas211 <faizalanas21@students.unnes.ac.id>
2026-02-12 11:01:54 +0700

    update

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -7,19 +7,38 @@
             </script>
             &nbsp;<strong>Magang Kemnaker</strong>
             <span class="mx-2">•</span>
-            <span>@fzlns21</span>
+
+            <a href="https://instagram.com/fzlns21" target="_blank" class="wm-link">
+                @fzlns21
+            </a>
+
             <span class="mx-1">|</span>
-            <span>@dhiyaind</span>
+
+            <a href="https://instagram.com/dhiyaind" target="_blank" class="wm-link">
+                @dhiyaind
+            </a>
         </div>
     </div>
 </footer>
+
 <style>
-    .footer-watermark {
-    font-size: 0.7rem;
-    color: #9ca3af;
-    opacity: 0.9;          /* sebelumnya 0.7 */
-    letter-spacing: 0.3px;
-    user-select: none;
+.footer-watermark{
+    font-size:0.7rem;
+    color:#9ca3af;
+    opacity:0.9;
+    letter-spacing:0.3px;
+    user-select:none;
 }
 
+/* link watermark */
+.footer-watermark .wm-link{
+    color:#9ca3af;
+    text-decoration:none;
+    transition:.2s;
+}
+
+.footer-watermark .wm-link:hover{
+    color:#6366f1;
+    text-decoration:underline;
+}
 </style>
